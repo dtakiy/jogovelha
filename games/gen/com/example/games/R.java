e772e2b92e0d134a818384c6d9afd389380b4535
Daniel Franco <tsuba12345@yahoo.com.br>
2014-09-17 23:58:30 -0300

    Jogo funcionando com p1 vs p2

--- games/gen/com/example/games/R.java
+++ games/gen/com/example/games/R.java
@@ -1079,8 +1079,10 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int background=0x7f020057;
         public static final int ic_launcher=0x7f020058;
+        public static final int icontest=0x7f020059;
     }
     public static final class id {
+        public static final int about=0x7f05004a;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1092,13 +1094,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050048;
+        public static final int action_settings=0x7f05004b;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int camporesultado=0x7f05003c;
+        public static final int camporesultado=0x7f050045;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int cpu=0x7f050049;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1119,17 +1122,18 @@ containing a value of this type.
         public static final int newgame=0x7f050046;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int p1=0x7f050048;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
-        public static final int quadrado1=0x7f050044;
-        public static final int quadrado2=0x7f050045;
-        public static final int quadrado3=0x7f050042;
-        public static final int quadrado4=0x7f050041;
-        public static final int quadrado5=0x7f05003e;
-        public static final int quadrado6=0x7f05003d;
-        public static final int quadrado7=0x7f050040;
-        public static final int quadrado8=0x7f05003f;
-        public static final int quadrado9=0x7f050043;
+        public static final int quadrado1=0x7f050043;
+        public static final int quadrado2=0x7f050044;
+        public static final int quadrado3=0x7f050041;
+        public static final int quadrado4=0x7f050040;
+        public static final int quadrado5=0x7f05003d;
+        public static final int quadrado6=0x7f05003c;
+        public static final int quadrado7=0x7f05003f;
+        public static final int quadrado8=0x7f05003e;
+        public static final int quadrado9=0x7f050042;
         public static final int radio=0x7f05002f;
         public static final int sair=0x7f050047;
         public static final int search_badge=0x7f050032;
@@ -1205,8 +1209,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_atividade2=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1252,10 +1257,13 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int about=0x7f0a0014;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int cpu=0x7f0a0013;
         public static final int hello_world=0x7f0a000e;
         public static final int newgame=0x7f0a0011;
+        public static final int p1=0x7f0a0012;
         public static final int sair=0x7f0a0010;
     }
     public static final class style {
